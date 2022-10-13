4812a9c1df4ceb0aa8ae4f9b63d90b92c4991033
H02c4n <svtr.hozcan@gmail.com>
2022-10-13 18:18:26 +0200

    Updates

---
+++ precache-manifest.e5a5eade36a63d682e1266edc140a442.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "03d20456e36e66d01883",
+    "url": "/lets-discover/static/css/main.2377544e.chunk.css"
+  },
+  {
+    "revision": "03d20456e36e66d01883",
+    "url": "/lets-discover/static/js/main.03d20456.chunk.js"
+  },
+  {
+    "revision": "5689c782e41a09b5d4a9",
+    "url": "/lets-discover/static/css/1.ed6fa722.chunk.css"
+  },
+  {
+    "revision": "5689c782e41a09b5d4a9",
+    "url": "/lets-discover/static/js/1.5689c782.chunk.js"
+  },
+  {
+    "revision": "d366d01c24b2c27ef11e",
+    "url": "/lets-discover/static/js/runtime~main.d366d01c.js"
+  },
+  {
+    "revision": "de5f31ccb40d60f89a8aa70842c116f2",
+    "url": "/lets-discover/index.html"
+  }
+];
